4eb7757522bbbc3561f86ec3102191355b038ef8
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-03-15 06:32:44 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      render json:students = Student.where("first_name LIKE ? OR last_name LIKE ?", "#{params[:name]}", "#{params[:name]}")
+    else
+      render json:students = Student.all
   end
+end
+  def show
+    studentInfo=Student.find(params[:id])
+    render json:studentInfo
+  end
+
 
 end
